e8eb4ea029208a00ae2c2edadc4271bc8f8c56e4
Sroza <87701060+Sroza@users.noreply.github.com>
2021-07-20 13:57:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-
+commandcreate("sa", "-userreply- As Hoşgeldin Bro!");
 csyhost.ready("Sroza test", "sroza");
 csyhost.login("o3b3w344s345c4z4n393a3u2x4p4r4w4o3c4g4s4j3g385r214z3z474o4h4b4z3d3264646n326g5s4e4e4e375r3b4v324r4r4j3h4v424x5e394x2q3l3r3p3t4b3t4h3t4h3n3j3i3o4b3s4q4u4o3l4r4h3f3e3h3m4j3r4f3h3f3c3x2x254c4c41303u223v2s203b4t2c4u284z2");
